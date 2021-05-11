720657aa85a8f3d165a591d0540e2427256658d1
dcoppage86 <dakotakcoppage@me.com>
2021-05-11 13:36:57 -0600

    removed user and added mood scaffold

--- big-me-backend/db/schema.rb
+++ big-me-backend/db/schema.rb
@@ -10,21 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_02_235233) do
-
-  create_table "charts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
