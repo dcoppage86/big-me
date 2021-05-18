2c2e4b906c124192fc6172954af51d986cc05ced
dcoppage86 <dakotakcoppage@me.com>
2021-05-17 23:22:29 -0600

    add DomContentLoaded event listener to index.js, added a function to get charts using fetch for a get request

--- big-me-frontend/src/index.js
+++ big-me-frontend/src/index.js
@@ -1,1 +1,13 @@
-console.log("this is my index.js");
+const baseUrl = "http://localhost:3000/"
+
+document.addEventListener('DOMContentLoaded',() => {
+    getChart()
+})
+
+function getChart() {
+    fetch(baseUrl + "charts")
+    .then(response => response.json())
+    .then(charts => {
+        console.log(charts);
+    })
+}
