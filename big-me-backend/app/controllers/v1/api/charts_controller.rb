b6502632ce18cd349f9a95cb182476ac8d7c0f99
dcoppage86 <dakotakcoppage@me.com>
2021-05-19 08:08:42 -0600

    created index and create routes and controller actions for chart

--- big-me-backend/app/controllers/v1/api/charts_controller.rb
+++ big-me-backend/app/controllers/v1/api/charts_controller.rb
@@ -1,2 +1,23 @@
 class V1::Api::ChartsController < ApplicationController
+    def index
+        charts = Chart.all
+        render json: charts
+    end
+
+    def create
+        chart = Chart.new(chart_params)
+        if chart.save 
+            render json: chart, status: :accepted
+        else
+            render json: {errors: chart.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private 
+    
+    def chart_params
+        params.require(chart).permit(:title, :content, :question_1, :question_2, :question_3, :question_4, :question_5, :question_6, :question_7, :question_8, :question_9, :question_10)
+    end
+    
+    
 end
