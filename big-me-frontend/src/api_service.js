3515fbae4a02eb7589dd998c962fb0510a8e3bfd
dcoppage86 <dakotakcoppage@me.com>
2021-05-23 12:19:41 -0600

    redoing index.js

--- big-me-frontend/src/api_service.js
+++ big-me-frontend/src/api_service.js
@@ -3,39 +3,16 @@
         this.baseUrl = "http://localhost:3000/api/v1"
     }
 
-    getCharts() {
-        return fetch(this.baseUrl + "/charts")
+    newChart(charts) {
+        return fetch(this.baseUrl + "/charts", {
+            method: "POST",
+            body: charts
+        })
         .then(response => response.json())
         .then(charts => {
-            charts.data.forEach(chart => {
-                const chartMarkup = `
-                    <div data-id=${chart.id}>
-                        <h3>${chart.attributes.title}</h3>
-                        <p>${chart.attributes.question_1}</p>
-                        <br>
-                        <p>${chart.attributes.question_2}</p>
-                        <br>
-                        <p>${chart.attributes.question_3}</p>
-                        <br>
-                        <p>${chart.attributes.question_4}</p>
-                        <br>
-                        <p>${chart.attributes.question_5}</p>
-                        <br>
-                        <p>${chart.attributes.question_6}</p>
-                        <br>
-                        <p>${chart.attributes.question_7}</p>
-                        <br>
-                        <p>${chart.attributes.question_8}</p>
-                        <br>
-                        <p>${chart.attributes.question_9}</p>
-                        <br>
-                        <p>${chart.attributes.question_10}</p>
-                    </div>`;
-    
-                document.querySelector('#chart-container').innerHTML += chartMarkup
-            
+            console.log("charts")
         })
-    })
+    }
 
 
-}}
+}
--- big-me-frontend/src/index.js
+++ big-me-frontend/src/index.js
@@ -1,6 +1,21 @@
-const api = new ApiService
+const baseUrl = "http://localhost:3000/api/v1"
 
 document.addEventListener('DOMContentLoaded',() => {
-    api.getCharts()
+    getCharts()
 })
+
+function getCharts() {
+    fetch(baseUrl + "/charts")
+    .then(response => response.json())
+    .then(charts => {
+        charts.data.forEach(chart => {
+        const chartMarkup = `
+        <div data-id=${chart.id}>
+            <h3>${chart.attributes.title}</h3>
+        </div>`;
+
+        document.querySelector('#chart-container').innerHTML += chartMarkup
+        })
+    })
+}
 
