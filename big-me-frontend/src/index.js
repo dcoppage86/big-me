e7abf2faaa6256cfaa36e9877da9f08545bf2e12
dcoppage86 <dakotakcoppage@me.com>
2021-05-25 11:04:03 -0600

    beginning the initial work on my post fetch

--- big-me-frontend/src/index.js
+++ big-me-frontend/src/index.js
@@ -12,6 +12,18 @@ function getCharts() {
         const chartMarkup = `
         <div data-id=${chart.id}>
             <h3>${chart.attributes.title}</h3>
+            <p>${chart.attributes.question_1}</p>
+            <p>${chart.attributes.question_2}</p>
+            <p>${chart.attributes.question_3}</p>
+            <p>${chart.attributes.question_4}</p>
+            <p>${chart.attributes.question_5}</p>
+            <p>${chart.attributes.question_6}</p>
+            <p>${chart.attributes.question_7}</p>
+            <p>${chart.attributes.question_8}</p>
+            <p>${chart.attributes.question_9}</p>
+            <p>${chart.attributes.question_10}</p>
+            <p hidden>${chart.attributes.mood_id}</p>
+            <img src=${chart.attributes.mood.image_url} height= 250 width=250></img>
         </div>`;
 
         document.querySelector('#chart-container').innerHTML += chartMarkup
