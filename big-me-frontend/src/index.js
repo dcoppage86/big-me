cfc0c90f8926672c0fb5c4a9bd69e4c2e512b615
dcoppage86 <dakotakcoppage@me.com>
2021-05-27 00:56:46 -0600

    changed my questions for my form to come from an array

--- big-me-frontend/src/index.js
+++ big-me-frontend/src/index.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded',() => {
     getCharts()
 })
 
+let questions = ["What special power are we feeling today?", "What did you dream about?", "What could we do today to be our best BigMe?", "What would be an awesome BigMe Reward?", "What is a green zone feeling today?", "What is a yellow zone feeling today?", "What is a blue zone feeling today?", "What is a red zone feeling today?", "What is the hardest thing you had to do yesterday?", "What is the best thing you did yesterday?"]
+
 function newChart() {
     fetch(baseUrl)
     .then(response => res)
@@ -21,34 +23,34 @@ function showCharts(charts) {
     charts.data.forEach(chart => {
         const chartMarkup = `
         <div data-id=${chart.id}>
-            <strong>${chart.attributes.question_1}</strong>
+            <strong>${questions[0]}</strong>
             <p>${chart.attributes.content_1}</p>
             <br>
-            <strong>${chart.attributes.question_2}</strong>
+            <strong>${questions[1]}</strong>
             <p>${chart.attributes.content_2}</p>
             <br>
-            <strong>${chart.attributes.question_3}</strong>
+            <strong>${questions[2]}</strong>
             <p>${chart.attributes.content_3}</p>
             <br>
-            <strong>${chart.attributes.question_4}</strong>
+            <strong>${questions[3]}</strong>
             <p>${chart.attributes.content_4}</p>
             <br>
-            <strong>${chart.attributes.question_5}</strong>
+            <strong>${questions[4]}</strong>
             <p>${chart.attributes.content_5}</p>
             <br>
-            <strong>${chart.attributes.question_6}</strong>
+            <strong>${questions[5]}</strong>
             <p>${chart.attributes.content_6}</p>
             <br>
-            <strong>${chart.attributes.question_7}</strong>
+            <strong>${questions[6]}</strong>
             <p>${chart.attributes.content_7}</p>
             <br>
-            <strong>${chart.attributes.question_8}</strong>
+            <strong>${questions[7]}</strong>
             <p>${chart.attributes.content_8}</p>
             <br>
-            <strong>${chart.attributes.question_9}</strong>
+            <strong>${questions[8]}</strong>
             <p>${chart.attributes.content_9}</p>
             <br>
-            <strong>${chart.attributes.question_10}</strong>
+            <strong>${questions[9]}</strong>
             <p>${chart.attributes.content_10}</p>
             <br>
             <p hidden>${chart.attributes.mood_id}</p>
