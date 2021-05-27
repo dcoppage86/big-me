d2545f6b18d400279f998db434a7dfcde158709d
dcoppage86 <dakotakcoppage@me.com>
2021-05-27 03:21:10 -0600

    updated seeds file

--- big-me-backend/db/seeds.rb
+++ big-me-backend/db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Mood.destroy_all
+
 mood = Mood.create([
     {feeling: "Happy", image_url: "https://i.ibb.co/d0kxphy/1-F601-color.png"},
     {feeling: "Sad", image_url: "https://i.ibb.co/cTgM0C2/1-F625-color.png"},
@@ -16,3 +18,6 @@ mood = Mood.create([
     {feeling: "Standoffish", image_url: "https://i.ibb.co/rZs48L8/1-F627-color.png"}])
 
    
+    chart = Chart.create([
+        {title: "Today was a great day", content: "Today we woke up, had breakfast and got ready for school without a single issue. Since we bought the special edition ASD shoes from the vans store, things have be so much easier in that department. Fyn brushed her hair, brushed her teeth, and despite some light frustration with wearing jeans we were able to navigate better. She even helped make her lunch for school!", date: "May 25, 2021", mood_id: 1}
+    ])
