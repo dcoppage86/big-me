343ea8af4bd90a52931ed712c789210cf8e6344c
dcoppage86 <dakotakcoppage@me.com>
2021-05-28 11:13:30 -0600

    refactored post and get functions

--- big-me-frontend/src/index.js
+++ big-me-frontend/src/index.js
@@ -22,44 +22,32 @@ function getCharts() {
 }
 
 function postCharts(title, content, date, mood_id) {
+    const bodyData = {title, content, date, mood_id}
     fetch(baseUrl, {
         // POST request
         method: "POST",
         headers: {"Content-Type": "application/json"},
-        body: JSON.stringify({
-            title: title, 
-            content: content, 
-            date: date, 
-            mood_id: mood_id
-        })
+        body: JSON.stringify(bodyData)
     })
     .then(response => response.json())
     .then(chart => {
-        const chartData = chart.data.attributes
-        const chartMarkup = `
-        <div data-id=${chartData.id}>
-            <strong>${chartData.date}: ${chart.attributes.title}</strong>
-            <p>${chartData.content}</p>
-            <br>
-            <p hidden>${chartData.mood_id}</p>
-            <img src=${chartData.mood.image_url} height= 100 width=100></img>
-        </div>`;
-
-        document.querySelector('#chart-container').innerHTML += chartMarkup
+        console.log(chart);
+        const chartData = chart.data
+        render(chartData)
     })
 }
 
 function render(chart) {
     const chartMarkup = `
-            <div data-id=${chart.id}>
-                <strong>${chart.attributes.date}: ${chart.attributes.title}</strong>
-                <p>${chart.attributes.content}</p>
-                <br>
-                <p hidden>${chart.attributes.mood_id}</p>
-                <img src=${chart.attributes.mood.image_url} height= 100 width=100></img>
-            </div>`;
+        <div data-id=${chart.id}>
+            <strong>${chart.attributes.date}: ${chart.attributes.title}</strong>
+            <p>${chart.attributes.content}</p>
+            <br>
+            <p hidden>${chart.attributes.mood_id}</p>
+            <img src=${chart.attributes.mood.image_url} height= 100 width=100></img>
+        </div>`;
     
-            document.querySelector('#chart-container').innerHTML += chartMarkup
+        document.querySelector('#chart-container').innerHTML += chartMarkup
 }
 
 
